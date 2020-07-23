6d9dfc70e5b1fbdbac9980238022437f61d4c5bd
Pablo Chico de Guzman <pchico83@gmail.com>
2020-07-23 06:33:44 +0200

    Switch message position

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,6 +1,6 @@
 module MessagesHelper
   def flex_position(message, &block)
-    if message.author == Current.username
+    if message.author != Current.username
       content_tag(:div, capture(&block), class: "d-flex flex-row-reverse")
     else
       content_tag(:div, capture(&block), class: "d-flex flex-row")
